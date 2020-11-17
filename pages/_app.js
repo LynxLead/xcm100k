31ef4713adfe697d78bb96f0560e250529b47c9c
LynxLead <lynxlead@gmail.com>
2020-11-17 14:33:06 +0100

    fix2p

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+    <title>XCM 100k</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
   return <Component {...pageProps} />
 }
 
